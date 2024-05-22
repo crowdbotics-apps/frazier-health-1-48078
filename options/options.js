ff4ba0db4f19f358053d8c23189daeb375a1440a
Crowdbotics <team@crowdbotics.com>
2024-05-22 23:23:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "frazier-health-1-48078",
+  url: "https://frazier-health-1-48078.botics.co",
+  api: "https://frazier-health-1-48078.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
